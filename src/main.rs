d2e7c4419afd4fdfb0ba00536f44724b09661d13
Jean Leonço <jeang.leonco@gmail.com>
2024-06-30 19:34:16 -0300

    handle concurrent clients

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,27 @@
-use std::{
-    io::{Read, Write},
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
     net::TcpListener,
 };
 
-fn main() {
+#[tokio::main]
+async fn main() {
     println!("Logs from your program will appear here!");
 
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    for stream in listener.incoming() {
-        match stream {
-            Ok(mut stream) => {
-                let mut buf = [0; 512];
-                loop {
-                    let bytes_read = stream.read(&mut buf).unwrap();
-                    if bytes_read == 0 {
-                        break;
-                    }
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
 
-                    stream.write_all(b"+PONG\r\n").unwrap();
+    loop {
+        let (mut stream, _) = listener.accept().await.unwrap();
+
+        tokio::spawn(async move {
+            let mut buf = [0; 1024];
+            loop {
+                let bytes_read = stream.read(&mut buf).await.unwrap();
+                if bytes_read == 0 {
+                    break;
                 }
+
+                stream.write_all(b"+PONG\r\n").await.unwrap();
             }
-            Err(e) => {
-                println!("error: {}", e);
-            }
-        }
+        });
     }
 }

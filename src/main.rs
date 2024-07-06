71736ce87d90f038ef3d6fbd9fb57db1e04f04e9
Jean Leonço <jeang.leonco@gmail.com>
2024-07-05 22:58:09 -0300

    move message to mod

--- src/main.rs
+++ src/main.rs
@@ -1,122 +1,10 @@
-use std::{
-    collections::HashMap,
-    fmt,
-    io::{BufRead, Cursor, Read},
-    sync::Arc,
-};
+use std::{collections::HashMap, io::Cursor, sync::Arc};
 
 use anyhow::{Context, Ok};
-use bytes::Buf;
-use tokio::{
-    io::{AsyncReadExt, AsyncWriteExt},
-    net::TcpListener,
-    sync::Mutex,
-};
+use message::Message;
+use tokio::{io::AsyncReadExt, net::TcpListener, sync::Mutex};
 
-#[derive(Debug, Clone)]
-enum Message {
-    Array { elements: Vec<Message> },
-    BulkString { data: String },
-    SimpleString { data: String },
-    Null,
-}
-
-impl fmt::Display for Message {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            Message::Array { .. } => write!(f, "Array"),
-            Message::BulkString { .. } => write!(f, "BulkString"),
-            Message::SimpleString { .. } => write!(f, "SimpleString"),
-            Message::Null { .. } => write!(f, "Null"),
-        }
-    }
-}
-
-impl Message {
-    fn from_buf(cursor: &mut Cursor<&[u8]>) -> anyhow::Result<Message> {
-        let first_byte = cursor.get_u8();
-
-        match first_byte {
-            b'*' => {
-                let mut size_buf = Vec::new();
-                cursor
-                    .read_until(b'\n', &mut size_buf)
-                    .context("Failed to read Array size")?;
-                let size: usize = std::str::from_utf8(&size_buf[..size_buf.len() - 2])
-                    .context("Failed to convert Array size to string")?
-                    .parse::<usize>()
-                    .context("Failed to convert Array size to usize")?;
-
-                let mut elements = Vec::with_capacity(size);
-                for i in 0..size {
-                    let message = Message::from_buf(cursor)
-                        .with_context(|| format!("Failed to parse Array element {}", i))?;
-                    elements.push(message);
-                }
-
-                Ok(Message::Array { elements })
-            }
-            b'$' => {
-                let mut size_buf = Vec::new();
-                cursor
-                    .read_until(b'\n', &mut size_buf)
-                    .context("Failed to read BulkString size")?;
-                let size: usize = std::str::from_utf8(&size_buf[..size_buf.len() - 2])
-                    .context("Failed to convert BulkString size to string")?
-                    .parse::<usize>()
-                    .context("Failed to convert BulkString size to usize")?;
-
-                let mut data = vec![0; size + 2];
-                Read::read_exact(cursor, &mut data).context("Failed to read BulkString data")?;
-
-                Ok(Message::BulkString {
-                    data: std::str::from_utf8(&data[..data.len() - 2])
-                        .context("Failed to parse BulkString data")?
-                        .to_string(),
-                })
-            }
-            b'+' => {
-                let mut data_buf: Vec<u8> = Vec::new();
-                cursor
-                    .read_until(b'\n', &mut data_buf)
-                    .context("Failed to read SimpleString data")?;
-
-                Ok(Message::SimpleString {
-                    data: std::str::from_utf8(&&data_buf[..data_buf.len() - 2])
-                        .context("Failed to parse SimpleString data")?
-                        .to_string(),
-                })
-            }
-            b'_' => {
-                // advance \r\n
-                cursor.advance(2);
-
-                Ok(Message::Null)
-            }
-            _ => anyhow::bail!("Message type not supported {}", first_byte),
-        }
-    }
-
-    fn serialize(self) -> Vec<u8> {
-        match self {
-            Message::Array { elements } => {
-                let mut buf: Vec<u8> = Vec::new();
-                buf.extend(format!("*{}\r\n", elements.len()).as_bytes());
-
-                for element in elements {
-                    buf.extend(element.serialize());
-                }
-
-                buf
-            }
-            Message::BulkString { data } => format!("${}\r\n{}\r\n", data.len(), data)
-                .as_bytes()
-                .to_vec(),
-            Message::SimpleString { data } => format!("+{}\r\n", data).as_bytes().to_vec(),
-            Message::Null => "_\r\n".as_bytes().to_vec(),
-        }
-    }
-}
+pub(crate) mod message;
 
 enum Command {
     Ping,
@@ -128,7 +16,7 @@ enum Command {
 impl Command {
     fn from_buf(buf: &mut [u8]) -> anyhow::Result<Command> {
         let message =
-            Message::from_buf(&mut Cursor::new(buf)).context("Failed to parse message")?;
+            Message::deserialize(&mut Cursor::new(buf)).context("Failed to parse message")?;
 
         // TODO: improve this
         match message {
@@ -216,30 +104,35 @@ async fn main() -> anyhow::Result<()> {
 
                 match command {
                     Command::Ping => {
-                        stream
-                            .write_all(b"+PONG\r\n")
+                        let message = Message::SimpleString {
+                            data: String::from("PONG"),
+                        };
+
+                        message
+                            .send(&mut stream)
                             .await
-                            .context("Failed to write PING response")?;
+                            .context("Failed to send PING reply")?;
                     }
                     Command::Echo { message } => {
-                        stream
-                            .write_all(&Message::BulkString { data: message }.serialize())
+                        let message = Message::SimpleString { data: message };
+
+                        message
+                            .send(&mut stream)
                             .await
-                            .context("Failed to write ECHO response")?;
+                            .context("Failed to send ECHO reply")?;
                     }
                     Command::Set { key, value } => {
                         let mut db = db.lock().await;
                         db.insert(key, value);
 
-                        stream
-                            .write_all(
-                                &Message::SimpleString {
-                                    data: String::from("OK"),
-                                }
-                                .serialize(),
-                            )
+                        let message = Message::SimpleString {
+                            data: String::from("OK"),
+                        };
+
+                        message
+                            .send(&mut stream)
                             .await
-                            .context("Failed to write Set response")?
+                            .context("Failed to send SET reply")?
                     }
                     Command::Get { key } => {
                         let db = db.lock().await;
@@ -252,10 +145,10 @@ async fn main() -> anyhow::Result<()> {
                             Message::Null
                         };
 
-                        stream
-                            .write_all(&message.serialize())
+                        message
+                            .send(&mut stream)
                             .await
-                            .context("Failed to write Get response")?;
+                            .context("Failed to send GET reply")?;
                     }
                 }
             }
---
+++ src/message.rs
@@ -0,0 +1,128 @@
+use std::{
+    fmt,
+    io::{BufRead, Cursor, Read},
+};
+
+use anyhow::Context;
+use bytes::Buf;
+use tokio::io::AsyncWriteExt;
+
+#[derive(Debug, Clone)]
+pub(crate) enum Message {
+    Array { elements: Vec<Message> },
+    BulkString { data: String },
+    SimpleString { data: String },
+    Null,
+}
+
+impl fmt::Display for Message {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Message::Array { .. } => write!(f, "Array"),
+            Message::BulkString { .. } => write!(f, "BulkString"),
+            Message::SimpleString { .. } => write!(f, "SimpleString"),
+            Message::Null { .. } => write!(f, "Null"),
+        }
+    }
+}
+
+const TERMINATOR_SIZE: usize = 2;
+
+impl Message {
+    pub(crate) fn deserialize(cursor: &mut Cursor<&[u8]>) -> anyhow::Result<Message> {
+        let first_byte = cursor.get_u8();
+
+        match first_byte {
+            b'*' => {
+                let size = parse_size(cursor).context("Failed to parse Array size")?;
+
+                let mut elements = Vec::with_capacity(size);
+                for i in 0..size {
+                    let message = Message::deserialize(cursor)
+                        .with_context(|| format!("Failed to parse Array element {}", i))?;
+                    elements.push(message);
+                }
+
+                Ok(Message::Array { elements })
+            }
+            b'$' => {
+                let size = parse_size(cursor).context("Failed to parse BulkString size")?;
+
+                let mut data = vec![0; size + TERMINATOR_SIZE];
+                cursor
+                    .read_exact(&mut data)
+                    .context("Failed to read BulkString data")?;
+
+                Ok(Message::BulkString {
+                    data: std::str::from_utf8(&data[..data.len() - TERMINATOR_SIZE])
+                        .context("Failed to parse BulkString data")?
+                        .to_string(),
+                })
+            }
+            b'+' => {
+                let mut data: Vec<u8> = Vec::new();
+                cursor
+                    .read_until(b'\n', &mut data)
+                    .context("Failed to read SimpleString data")?;
+
+                Ok(Message::SimpleString {
+                    data: std::str::from_utf8(&&data[..data.len() - TERMINATOR_SIZE])
+                        .context("Failed to parse SimpleString data")?
+                        .to_string(),
+                })
+            }
+            b'_' => {
+                cursor.advance(TERMINATOR_SIZE);
+
+                Ok(Message::Null)
+            }
+            _ => anyhow::bail!("Unknown message type {}", first_byte),
+        }
+    }
+
+    pub(crate) fn serialize(self) -> Vec<u8> {
+        match self {
+            Message::Array { elements } => {
+                let mut buf: Vec<u8> = Vec::new();
+                buf.extend(format!("*{}\r\n", elements.len()).as_bytes());
+
+                for element in elements {
+                    buf.extend(element.serialize());
+                }
+
+                buf
+            }
+            Message::BulkString { data } => format!("${}\r\n{}\r\n", data.len(), data)
+                .as_bytes()
+                .to_vec(),
+            Message::SimpleString { data } => format!("+{}\r\n", data).as_bytes().to_vec(),
+            Message::Null => "_\r\n".as_bytes().to_vec(),
+        }
+    }
+
+    pub(crate) async fn send(
+        self,
+        writer: &mut (impl AsyncWriteExt + std::marker::Unpin),
+    ) -> anyhow::Result<()> {
+        writer
+            .write_all(&self.serialize())
+            .await
+            .context("Failed to send reply")?;
+
+        Ok(())
+    }
+}
+
+fn parse_size(cursor: &mut Cursor<&[u8]>) -> anyhow::Result<usize> {
+    let mut size_buf = Vec::new();
+    cursor
+        .read_until(b'\n', &mut size_buf)
+        .context("Failed to read size")?;
+
+    let size = std::str::from_utf8(&size_buf[..size_buf.len() - 2])
+        .context("Failed to convert size to string")?
+        .parse::<usize>()
+        .context("Failed to parse size to usize")?;
+
+    Ok(size)
+}
